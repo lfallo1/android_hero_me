4c320ecbbb003e468024563e5a408d4c9070c6b1
Lance Fallon <lfallon@Lances-Mac-mini.local>
2016-08-23 11:46:39 -0400

    initial commit

---
+++ app/src/main/java/com/training/lfallon/androidfragmentstut/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.training.lfallon.androidfragmentstut.activities;
+
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.training.lfallon.androidfragmentstut.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
